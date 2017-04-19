36c9087dec201f4f59fad301c61147d39de85fa8
yoann-dufresne <yoann.dufresne0@gmail.com>
2017-04-19 14:47:47 +0200

    basic options

--- wordborhood.cpp
+++ wordborhood.cpp
@@ -14,16 +14,19 @@ using namespace std;
 int main (int argc, char *argv[]) {
 	int k = 1;
 	bool verbose = false;
+	int alphabet_size = 4;
 
 	// Too few arguments
-	if (argc <= 2) {
+	if (argc < 4) {
 		cerr << "Wrong arguments. The command line must be like:" << endl;
-		cerr << "./wordbourhood <word> <distance> [options]" << endl;
+		cerr << "./wordbourhood <word> <distance> <alphabet size> [options]" << endl;
+		cerr << "Options:" << endl;
+		cerr << "-v : Verbose" << endl;
 		return -1;
 	}
 
 	// 
-	for (int i=3 ; i<argc ; i++) {
+	for (int i=4 ; i<argc ; i++) {
 		char * arg = argv[i];
 
 		if (arg[0] != '-')
@@ -35,6 +38,7 @@ int main (int argc, char *argv[]) {
 
 	k = stoi(argv[2]);
 	string word (argv[1]);
+	alphabet_size = stoi(argv[3]);
 
 	if (verbose)
 		cout << "Create automata" << endl;
@@ -44,7 +48,7 @@ int main (int argc, char *argv[]) {
 
 	if (verbose)
 		cout << "Couting the neighborhood of " << word << " with a distance of " << to_string(k) << endl;
-	long long count = count_neighbors (dul, enc, k, word.size(), 3);
+	long long count = count_neighbors (dul, enc, k, word.size(), alphabet_size);
 
 	cout << count << endl;
 
